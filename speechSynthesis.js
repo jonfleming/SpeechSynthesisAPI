7c04f31f038754c165c6b9f8c930fe745e85f047
Jon Fleming <jonfleming@hotmail.com>
2020-03-23 03:15:39 -0700

    Extract javascript

--- speechSynthesis.js
+++ speechSynthesis.js
@@ -2,51 +2,50 @@ var synth = window.speechSynthesis;
 var available_voices;
 
 // list of languages is probably not loaded, wait for it
-if(window.speechSynthesis.getVoices().length == 0) {
-  window.speechSynthesis.addEventListener('voiceschanged', function() {
+if (window.speechSynthesis.getVoices().length == 0) {
+    window.speechSynthesis.addEventListener('voiceschanged', function() {
+        available_voices = window.speechSynthesis.getVoices();
+    });
+} else {
     available_voices = window.speechSynthesis.getVoices();
-  });
-}
-else {
-  available_voices = window.speechSynthesis.getVoices();
 }
 
- function textToSpeech(text) {
-  // get all voices that browser offers
-  var available_voices = window.speechSynthesis.getVoices();
+function textToSpeech(text) {
+    // get all voices that browser offers
+    var available_voices = window.speechSynthesis.getVoices();
 
-  // this will hold an english voice
-  var english_voice = '';
+    // this will hold an english voice
+    var english_voice = '';
 
-  // find voice by language locale "en-US"
-  // if not then select the first voice
-  for(var i=0; i<available_voices.length; i++) {
-//    alert(available_voices[i].lang + ' ' + available_voices[i].name);
-    if(available_voices[i].lang === 'en-US' && available_voices[i].name === 'Samantha') {
-      english_voice = available_voices[i];
-      break;
+    // find voice by language locale "en-US"
+    // if not then select the first voice
+    for (var i = 0; i < available_voices.length; i++) {
+        //    alert(available_voices[i].lang + ' ' + available_voices[i].name);
+        if (available_voices[i].lang === 'en-US' && available_voices[i].name === 'Samantha') {
+            english_voice = available_voices[i];
+            break;
+        }
     }
-  }
-  if(english_voice === '')
-    english_voice = available_voices[0];
+    if (english_voice === '')
+        english_voice = available_voices[0];
 
-  // new SpeechSynthesisUtterance object
-  var utter = new SpeechSynthesisUtterance();
-  utter.rate = 1;
-  utter.pitch = 0.5;
-  utter.text = text;
-  utter.voice = english_voice;
+    // new SpeechSynthesisUtterance object
+    var utter = new SpeechSynthesisUtterance();
+    utter.rate = 1;
+    utter.pitch = 0.5;
+    utter.text = text;
+    utter.voice = english_voice;
 
-  // event after text has been spoken
-  utter.onend = function() {
-//      alert('Speech has finished');
-  }
+    // event after text has been spoken
+    utter.onend = function() {
+        //      alert('Speech has finished');
+    }
 
-  // speak
-  window.speechSynthesis.speak(utter);
-}     
+    // speak
+    window.speechSynthesis.speak(utter);
+}
 
 function say(id) {
-  var text = document.getElementById(id).value;
-  textToSpeech(text);
+    var text = document.getElementById(id).value;
+    textToSpeech(text);
 }
